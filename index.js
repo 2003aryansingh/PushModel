29fd8ccf9a96789fcbd47e800ab69f7b735516f1
2003aryansingh <s424.aryansingh@gmail.com>
2023-04-18 21:09:46 +0530

    Added the client code

--- index.js
+++ index.js
@@ -23,3 +23,6 @@ websocket.on("request", request => {
 });
 
 
+//let ws = new WebSocket("ws://localhost:8080");
+//ws.onmessage = message => console.log(`Received: ${message.data}`);
+//ws.send("Hello! I'm client")
